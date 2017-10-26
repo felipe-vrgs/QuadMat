e13a46cc498ad87e78e61726374431f6ecf5f02c
vargones <feliipevrgs@gmail.com>
2017-10-26 14:22:21 -0200

    Simulink model

--- src/cacm.m
+++ src/cacm.m
@@ -58,6 +58,27 @@ function SetGlobals()
         fprintf('[%g] = %g a %g\n',n,x2ini+(n-1)*x2delta,x2ini+n*x2delta);
     end
     %}
+    
+    % Ctes do modelo
+    global g, L, Kf, Km, m, a, b;
+    g = - 9.81;       % Gravidade
+    Ix = 5e-3;      % Inércia eixo X
+    Iy = 5e-3;      % Inércia eixo Y
+    Iz = 10e-3;     % Inércia eixo Z
+    L = 0.25;       % Distância do centro até qualquer um dos motores
+    Km = 3e-6;      % Cte aerodinâmica (thrust)
+    Kf = 1e-7;      % Cte de arrasto (drag)
+    m = 0.5;        % Massa do drone
+    Jr = 6e-5;      % Inércia do rotor
+    % Redução de variáveis
+    a(1) = (Iy - Iz)/Ix;
+    a(2) = Jr/Ix;
+    a(3) = (Iz - Ix)/Iy;
+    a(4) = Jr/Iy;
+    a(5) = (Ix - Iy)/Iz;
+    b(1) = L/Ix;
+    b(2) = L/Iy;
+    b(3) = L/Iz;
 end
 
 
@@ -122,38 +143,20 @@ function dx = Quadricopter(t,x)
         x(11) => y
         x(12) => dy/dT
     %}
-    
-    % Ctes
-    g = - 9.81;       % Gravidade
-    Ix = 5e-3;      % Inércia eixo X
-    Iy = 5e-3;      % Inércia eixo Y
-    Iz = 10e-3;     % Inércia eixo Z
-    L = 0.25;       % Distância do centro até qualquer um dos motores
-    d = 3e-6;       % Cte aerodinâmica (thrust)
-    b = 1e-7;       % Cte de arrasto (drag)
-    m = 0.5;        % Massa do drone
-    Jr = 6e-5;      % Inércia do rotor
+    global g, Km, b, m, a, Kf;
 
     % W é a vel angular em cada motor
     W = [6000 5000 6000 7000];
     % W = [0 0 0 0];
 
     % Matriz U das entradas
-    % U(1) = b*(W(1)^2 + W(2)^2 + W(3)^2 + W(4)^2);   % Total Thrust
-    % U(2) = b*(W(4)^2 - W(2)^2);                     % Roll
-    % U(3) = b*(- W(3)^2 + W(1)^2);                   % Pitch
-    % U(4) = d*(W(1)^2 - W(2)^2 + W(3)^2 - W(4)^2);   % Yaw
+    % U(1) = Kf*(W(1)^2 + W(2)^2 + W(3)^2 + W(4)^2);   % Total Thrust
+    % U(2) = Kf*(W(4)^2 - W(2)^2);                     % Roll
+    % U(3) = Kf*(- W(3)^2 + W(1)^2);                   % Pitch
+    % U(4) = Km*(W(1)^2 - W(2)^2 + W(3)^2 - W(4)^2);   % Yaw
     U = [5 0 2 0];
                                                 
-    % Redução de variáveis
-    a(1) = (Iy - Iz)/Ix;
-    a(2) = Jr/Ix;
-    a(3) = (Iz - Ix)/Iy;
-    a(4) = Jr/Iy;
-    a(5) = (Ix - Iy)/Iz;
-    b(1) = L/Ix;
-    b(2) = L/Iy;
-    b(3) = L/Iz;
+  
     
     % Omega é a velocidade relativa do motor
     Omega = W(1) - W(2) + W(3) - W(4);
